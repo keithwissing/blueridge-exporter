33e2a058a715a0b60de0de6e4f81b6777ecc7c8d
Keith Wissing <hitiek@xorad.com>
2022-05-12 14:57:04 -0400

    add healthcheck program

--- src/healthcheck.py
+++ src/healthcheck.py
@@ -1,5 +1,13 @@
+import os
+import urllib
+import urllib.request
+
 def main():
-    pass
+    port = os.getenv('EXPORTER_PORT', 9798)
+    with urllib.request.urlopen(f'http://127.0.0.1:{port}/') as f:
+        body = f.read(100).decode('utf-8')
+    assert 'Blueridge-Exporter' in body
+    assert 'Metrics' in body
 
 if __name__ == "__main__":
     main()
